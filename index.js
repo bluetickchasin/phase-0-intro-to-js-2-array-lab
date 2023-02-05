bb6f89db2b5862f030af1821f869fbc353a5abe1
Laurence Fairbanks <laurencefairbanks@yahoo.com>
2023-02-04 19:15:43 -0600

    commit tree 3972ed4c6fc4c89200273bd89fec9e8e3560e3c5

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function  destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+   const newCatArray = [...cats, name]
+   return newCatArray
+}
+
+function prependCat(name) {
+   const newCatArray = [name, ...cats]
+   return newCatArray
+}
+
+function removeLastCat() {
+   const copyCats = cats.slice(0, -1)
+    return copyCats
+}
+
+function removeFirstCat() {
+const copyCats = cats.slice(1)
+return copyCats
+}
